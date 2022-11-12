8c8775591bd4978915c613e2803f24c98d045668
Deniz <deniz.a.kalkan@hotmail.co.uk>
2022-11-12 04:00:02 +0000

    Lvl req lowered for boiled sausage

--- AutoGen/Food/BoiledSausage.override.cs
+++ AutoGen/Food/BoiledSausage.override.cs
@@ -55,7 +55,7 @@ namespace Eco.Mods.TechTree
     /// This is an auto-generated class. Don't modify it! All your changes will be wiped with next update! Use Mods* partial methods instead for customization. 
     /// If you wish to modify this class, please create a new partial class or follow the instructions in the "UserCode" folder to override the entire file.
     /// </remarks>
-    [RequiresSkill(typeof(AdvancedCookingSkill), 2)]
+    [RequiresSkill(typeof(AdvancedCookingSkill), 1)]
     public partial class BoiledSausageRecipe : RecipeFamily
     {
         public BoiledSausageRecipe()
